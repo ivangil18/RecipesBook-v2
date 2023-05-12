00aa9e45b2ea670187972a8c7a8142913aab21ab
Ivan Gil <igil.dev@gmail.com>
2023-05-12 12:14:36 -0400

    components restructured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RecipeListComponent } from './components/recipe/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './components/recipe/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './components/recipe/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './components/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipes/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './components/shopingList/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './components/shopingList/shopping-list-edit/shopping-list-edit.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { HeaderComponent } from './components/header/header.component';
     ShoppingListComponent,
     ShoppingListEditComponent,
     HeaderComponent,
+    RecipesComponent,
   ],
   imports: [BrowserModule],
   providers: [],
